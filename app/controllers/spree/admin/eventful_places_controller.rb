8932a90844530db1342619b5131c8a298a4d4e72
Paweł Strzałkowski <pawel@praesens.co>
2018-02-23 10:39:06 +0100

    Location after save

--- app/controllers/spree/admin/eventful_places_controller.rb
+++ app/controllers/spree/admin/eventful_places_controller.rb
@@ -1,6 +1,11 @@
 module Spree
   module Admin
     class EventfulPlacesController < ResourceController
+      protected
+
+      def location_after_save
+        edit_admin_eventful_place_path
+      end
     end
   end
 end
